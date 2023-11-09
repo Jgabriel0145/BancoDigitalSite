1f4a9dd645ba28c28ed731149fa0d499b04f375c
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-11-09 15:50:20 -0300

    Alterando Navbar.

--- App/View/PaginaInicialPropaganda.php
+++ App/View/PaginaInicialPropaganda.php
@@ -8,29 +8,24 @@
     <?php include VIEWS . 'Assets/css/PaginaInicialPropaganda/paginainicialpropaganda.php'; ?>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="/View/Assets/Img/Geral/logo_sem_fundo.png" id="logo-navbar">
-            </a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-sm navbar-light" id="neubar">
+        <div class="container">
+            <a class="navbar-brand" href="#"><!--<img src="/View/Assets/Img/PagInicialPropaganda/logo.png" height="50" />--></a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+    
+            <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto ">
+                    <li class="nav-item">
+                      <a class="nav-link mx-2 active" aria-current="page" href="/login">Login</a>
+                    </li>
+            
+                    <li class="nav-item">
+                        <a class="nav-link mx-2 active" aria-current="page" href="/correntista/cadastro">Cadastre-se</a>
+                    </li>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/correntista/cadastro">
-                            Cadastrar
-                        </a>
-                    </li>
-                    
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/login">
-                            Login
-                        </a>
-                    </li>
                 </ul>
             </div>
         </div>
