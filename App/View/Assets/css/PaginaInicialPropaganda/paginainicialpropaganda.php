022f46946017023f0705c8c614f01f77225012cc
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-11-13 13:47:32 -0300

    Atualização da interface de propaganda.

--- App/View/Assets/css/PaginaInicialPropaganda/paginainicialpropaganda.php
+++ App/View/Assets/css/PaginaInicialPropaganda/paginainicialpropaganda.php
@@ -1,35 +1,91 @@
 <style>
-    #neubar {
-      background: #FC7C04;
-      box-shadow: 4px 6px 12px rgba(0,0,0,0.4)
-    }
-    
-    /*.nav-item .active {
-      border-radius: 6px;
-      background: linear-gradient(145deg, #ffe7ca, #f5d7b2);
-      box-shadow:  4px 4px 8px #ddc1a0,
-                   -4px -4px 8px #f7e5cc;
-    }
-    #neubar .dropdown-menu a:hover {
-      color: #454545
-    }
-    #neubar .nav-item {
-      margin : auto 4px;
-    }
-    #neubar a {
-    
-      padding-left:12px;
-      padding-right:12px;
-    }
-    #neubar .dropdown-menu {
-      background : #545454
-    }
-    a.navbar-brand {
-      color: #454545
-    }*/
-    #neubar .nav-item a
+    *
     {
-        color: #FFFFFF;
-        font: 1.3em arial;
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-family: Arial, Helvetica, sans-serif;
     }
+
+    body
+    {
+        background-color: #FFFFFF;
+        min-width: 500px;   
+    }
+    /*
+    .navbar
+    {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        width: 98%;
+        padding: 1%;
+        background-color: #FC7C04;
+        min-width: 500px;
+    }
+
+    .navbar img
+    {
+        width: 8%;
+        min-width: 100px;
+    }
+
+    .navbar-botoes
+    {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .navbar-botoes button
+    {
+        width: 125px;
+        padding: 1%;
+        margin: 1%;
+        font-size: 1.1em;
+        border-radius: 5px;
+        background-color: rgba(0, 0, 0, 0);
+        font-weight: bold;
+        color: #FFFFFF;
+        text-decoration: underline;
+    }*/
+
+    .navbar
+    {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        background-color: #FC7C04;
+        padding: 1%;
+    }
+
+    .navbar img
+    {
+        width: 8%;
+        min-width: 100px;
+    }
+
+    .navbar-botoes
+    {
+        display: flex;
+        flex-direction: row;
+        justify-self: right;
+    }
+
+    .navbar-botoes button
+    {
+        width: 125px;
+        padding: 1%;
+        margin: 1%;
+        font-size: 1.1em;
+        border-radius: 5px;
+        background-color: rgba(0, 0, 0, 0);
+        font-weight: bold;
+        color: #FFFFFF;
+        text-decoration: underline;
+    }
+
+    .navbar-botoes button:hover
+    {
+        cursor: pointer;
+    }
+
 </style>
