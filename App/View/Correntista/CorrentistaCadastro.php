10c2fe03225d3c7821e2446a7dba14b98edcda5a
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-11-16 15:44:34 -0300

    Interface de cadastro.

--- App/View/Correntista/CorrentistaCadastro.php
+++ App/View/Correntista/CorrentistaCadastro.php
@@ -1,19 +1,78 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inter | Cadastro</title>
+    <link rel="icon" href="/View/Assets/Img/Geral/logo_icone.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <form action="/correntista/cadastro/save" method="post">
-        <input type="text" name="nome" placeholder="Nome">
-        <input type="number" name="cpf" placeholder="CPF" maxlength="11" minlength="11">
-        <input type="email" name="email" placeholder="Email">
-        <input type="date" name="data_nasc">
-        <input type="password" name="senha" placeholder="Senha">
 
-        <button type="submit">Cadastrar</button>
-    </form>
+<section class="vh-100" style="background-color: #FC7C04;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-xl-10">
+                <div class="card" style="border-radius: 1rem;">
+                <div class="row g-0">
+                    <div class="col-md-6 col-lg-5 d-none d-md-block">
+                    <img src="/View/Assets/Img/Login/celular.png" style="width: 120%; height: 100%;"
+                        alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                    </div>
+                    <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div class="card-body p-4 p-lg-5 text-black">
+
+                        <form action="/correntista/cadastro/save" method="post">
+
+                        <div class="d-flex align-items-center mb-3 pb-1">
+                            <i class="fas fa-cubes fa-2x me-3" style="color: #FC7C04;"></i>
+                            <span class="h1 fw-bold mb-0">Cadastro</span>
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <input type="text" id="form2Example17" class="form-control form-control-lg" name="nome"/>
+                            <label class="form-label" for="form2Example17">Nome</label>
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <input type="text" id="form2Example17" class="form-control form-control-lg" name="cpf" maxlength="14"/>
+                            <label class="form-label" for="form2Example17">CPF</label>
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <input type="email" id="form2Example17" class="form-control form-control-lg" name="email"/>
+                            <label class="form-label" for="form2Example17">Email</label>
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <input type="date" id="form2Example17" class="form-control form-control-lg" name="data_nasc"/>
+                            <label class="form-label" for="form2Example17">Data de Nascimento</label>
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <input type="password" id="form2Example27" class="form-control form-control-lg" name="senha"/>
+                            <label class="form-label" for="form2Example27">Senha</label>
+                        </div>
+
+                        <div class="pt-1 mb-4">
+                            <button class="btn btn-lg btn-block" type="submit" style="background-color: #FC7C04; color: #FFFFFF;">Cadastrar</button>
+                        </div>
+
+                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Já possui conta? <a href="/login"
+                            style="color: #393f81;">Entrar</a></p>
+                        </form>
+
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
 </html>
+
--- App/View/Login/Login.php
+++ App/View/Login/Login.php
@@ -6,7 +6,6 @@
     <title>Inter | Login</title>
     <link rel="icon" href="/View/Assets/Img/Geral/logo_icone.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/inputmask@5.0.3/dist/jquery.inputmask.min.js"></script>
 </head>
 <body>
     <section class="vh-100" style="background-color: #FC7C04;">
